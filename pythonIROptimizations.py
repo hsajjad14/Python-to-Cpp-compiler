3b18595a84906d7a66a6ec252f8637bae3ffb242
hsajjad14 <haider.sajjad@mail.utoronto.ca>
2024-02-26 13:14:16 -0500

    .

--- pythonIROptimizations.py
+++
@@ -1,9 +0,0 @@
-from pythonConstantFolding import ConstantFolding
-
-class IROptimizer(object):
-
-    def __init__(self, IR_lst, st):
-        self.constantFoldingOptimizer = ConstantFolding(IR_lst, st)
-
-    def do_constant_folding(self):
-        return self.constantFoldingOptimizer.do_constant_folding()
